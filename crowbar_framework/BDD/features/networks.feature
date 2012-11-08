2d4768b71555d020612e8b5c45496399817bf823
cdearborn <Christopher_Dearborn@dell.com>
2012-11-08 15:03:56 -0500

    Refactored validate method to use crowbar_rest:validate. Started adding in network list test.

--- crowbar_framework/BDD/features/networks.feature
+++ crowbar_framework/BDD/features/networks.feature
@@ -2,6 +2,10 @@ Feature: Networks
   In order to manage networks through the network API A remote program
   wants to be able to list, show, create, edit, and delete networks
 
+  Scenario: %Retrieve the list of networks via the network API
+    When REST requests the list of networks
+    Then the object id list is properly formatted
+  
   Scenario: Retrieve a network via the network API
     When REST requests the network "admin"
     Then the network is properly formatted
--- crowbar_framework/BDD/networks.erl
+++ crowbar_framework/BDD/networks.erl
@@ -1,6 +1,13 @@
 -module(networks).
 -export([step/3, validate/1]).
 
+% This method is used to define constants
+g(Item) ->
+  case Item of
+    path -> "2.0/crowbar/2.0/network/networks";
+    _ -> crowbar:g(Item)
+  end.
+
 
 rangeTester(_Range) -> 
   { _Index, _Content } = _Range,
@@ -18,52 +25,50 @@ rangeTester(_Range) ->
 
 validate(JSON) ->
   RangeTester = fun(Value) -> rangeTester(Value) end,
-  try JSON of
-    J ->
-        {"created_at", _CreatedAt} = lists:keyfind("created_at", 1, JSON),
-        {"updated_at",_UpdatedAt} = lists:keyfind("updated_at", 1, JSON), 
-        {"dhcp_enabled",_DhcpEnabled} = lists:keyfind("dhcp_enabled", 1, JSON),
-        {"id",_Id} = lists:keyfind("id", 1, JSON),
-        {"proposal_id",_ProposalId} = lists:keyfind("proposal_id", 1, JSON),
-        {"conduit_id",_ConduitId} = lists:keyfind("conduit_id", 1, JSON),
-        {"name",_Name} = lists:keyfind("name", 1, JSON), 
+  try 
+    {"dhcp_enabled",_DhcpEnabled} = lists:keyfind("dhcp_enabled", 1, JSON),
+    {"proposal_id",_ProposalId} = lists:keyfind("proposal_id", 1, JSON),
+    {"conduit_id",_ConduitId} = lists:keyfind("conduit_id", 1, JSON),
 
-        {"subnet",_Subnet}  = lists:keyfind("subnet", 1, JSON), 
-        {"cidr",_SubnetAddr}  = lists:keyfind("cidr", 1, _Subnet), 
+    {"subnet",_Subnet}  = lists:keyfind("subnet", 1, JSON), 
+    {"cidr",_SubnetAddr}  = lists:keyfind("cidr", 1, _Subnet), 
 
-        {"router",_Router}  = lists:keyfind("router", 1, JSON), 
-        {"ip",_RouterIp}  = lists:keyfind("ip", 1, _Router), 
-        {"cidr",_RouterAddr}  = lists:keyfind("cidr", 1, _RouterIp), 
-        {"pref",_RouterPref}  = lists:keyfind("pref", 1, _Router), 
+    {"router",_Router}  = lists:keyfind("router", 1, JSON), 
+    {"ip",_RouterIp}  = lists:keyfind("ip", 1, _Router), 
+    {"cidr",_RouterAddr}  = lists:keyfind("cidr", 1, _RouterIp), 
+    {"pref",_RouterPref}  = lists:keyfind("pref", 1, _Router), 
 
-        {"ip_ranges",_IpRanges}  = lists:keyfind("ip_ranges", 1, JSON), 
-        _RangeR = lists:map( RangeTester, _IpRanges),
+    {"ip_ranges",_IpRanges}  = lists:keyfind("ip_ranges", 1, JSON), 
+    _RangeR = lists:map( RangeTester, _IpRanges),
 
-        R = [bdd_utils:is_a(boolean, _DhcpEnabled),
-             bdd_utils:is_a(dbid, _Id),
-             bdd_utils:is_a(dbid, _ProposalId),
+    R = [bdd_utils:is_a(boolean, _DhcpEnabled),
+         bdd_utils:is_a(dbid, _ProposalId),
              bdd_utils:is_a(dbid, _ConduitId),
-             bdd_utils:is_a(name, _Name),
-             bdd_utils:is_a(cidr, _SubnetAddr),
-             bdd_utils:is_a(ip, _RouterAddr),
-             bdd_utils:is_a(number, _RouterPref),
-             _RangeR
-           ],
-        FlatteR = lists:flatten(R),
+         bdd_utils:is_a(cidr, _SubnetAddr),
+         bdd_utils:is_a(ip, _RouterAddr),
+         bdd_utils:is_a(number, _RouterPref),
+         _RangeR,
+         crowbar_rest:validate(JSON)
+       ],
+    FlatteR = lists:flatten(R),
 
-        case bdd_utils:assert(FlatteR)of
-          true -> true;
-          false -> io:format("FAIL: JSON did not comply with object format ~p~n", [JSON]), false
-        end
+    case bdd_utils:assert(FlatteR)of
+      true -> true;
+      false -> io:format("FAIL: JSON did not comply with object format ~p~n", [JSON]), false
+    end
   catch
-    X: Y -> io:format("ERROR: unable to parse returned network JSON: ~p:~p~n", [X, Y]),
-		false
+    X: Y -> io:format("ERROR: unable to parse returned network JSON: ~p:~p~n", [X, Y]), false
 	end. 
+
+
+% List networks
+step(_Config, _Given, {step_when, _N, ["REST requests the list of networks"]}) ->
+  bdd_restrat:step(_Config, _Given, {step_when, _N, ["REST requests the", g(path),"page"]});
 
 
 % Retrieve a network
 step(_Config, _Given, {step_when, _N, ["REST requests the network",Name]}) ->
-  bdd_restrat:step(_Config, _Given, {step_when, _N, ["REST requests the", eurl:path("2.0/crowbar/2.0/network/networks",Name),"page"]});
+  bdd_restrat:step(_Config, _Given, {step_when, _N, ["REST requests the", eurl:path(g(path),Name),"page"]});
 
 step(_Config, Result, {step_then, _N, ["the network is properly formatted"]}) ->
   crowbar_rest:step(_Config, Result, {step_then, _N, ["the", networks, "object is properly formatted"]});
@@ -71,7 +76,7 @@ step(_Config, Result, {step_then, _N, ["the network is properly formatted"]}) ->
 
 % Delete a network
 step(Config, _Given, {step_when, _N, ["REST removes the network",Network]}) ->
-  eurl:delete(Config, "2.0/crowbar/2.0/network/networks", Network);
+  eurl:delete(Config, g(path), Network);
 
 step(Config, _Result, {step_then, _N, ["there is not a network",Network]}) -> 
-  crowbar_rest:get_id(Config, "2.0/crowbar/2.0/network/networks", Network) == "-1".
+  crowbar_rest:get_id(Config, g(path), Network) == "-1".
