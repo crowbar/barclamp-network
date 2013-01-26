def2e22a9fdcf5e11fc4d4c47b212e46c1995998
David Paterson <david_paterson@dell.com>
2013-01-25 20:04:34 -0500

    Condensing network navigation items into single migration in network barclamp

--- crowbar_framework/db/migrate/20130115160000_network_navs.rb
+++ crowbar_framework/db/migrate/20130115160000_network_navs.rb
@@ -17,13 +17,15 @@ class NetworkNavs2 < ActiveRecord::Migration
   def self.up
     Nav.find_or_create_by_item :item=>'network', :parent_item=>'root', :name=>'nav.network', :description=>'nav.network_description', :path=>"networks_path", :order=>2000, :development=>true
     Nav.find_or_create_by_item :item=>'networks', :parent_item=>'network', :name=>'nav.networks', :description=>'nav.networks_description', :path=>"networks_path", :order=>100
-    Nav.find_or_create_by_item :item=>'conduits', :parent_item=>'network', :name=>'nav.conduits', :description=>'nav.conduits_description', :path=>"conduits_path", :order=>200
+    Nav.find_or_create_by_item :item=>'switches', :parent_item=>'network', :name=>'nav.switch', :description=>'nav.switch_description', :path=>"switch_path", :order=>200
+    Nav.find_or_create_by_item :item=>'vlan', :parent_item=>'network', :name=>'nav.vlan', :description=>'nav.vlan_description', :path=>"vlan_path", :order=>300
 
   end
 
   def self.down
     Nav.delete_by_item 'networks'
-    Nav.delete_by_item 'conduits'
+    Nav.delete_by_item 'switches'
+    Nav.delete_by_item 'vlan'
     Nav.delete_by_item 'network'
   end
 end
