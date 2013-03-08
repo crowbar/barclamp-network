4937e88d1039adbcb296148a91e480899b3abe5f
David Paterson <david_paterson@dell.com>
2013-03-08 07:04:01 -0500

    Fixing up networks controller to use new v2 pattern where possible

--- crowbar_engine/barclamp_network/app/models/barclamp_network/network_utils.rb
+++ crowbar_engine/barclamp_network/app/models/barclamp_network/network_utils.rb
@@ -54,6 +54,36 @@ class BarclampNetwork::NetworkUtils
 
     [200, network]
   end
+  
+ def self.get_networks(
+    deployment_id = Barclamp::DEFAULT_DEPLOYMENT_NAME,
+    snapshot_type = PROPOSED_SNAPSHOT)
+
+    networks = nil
+    net_bc_id = BarclampNetwork::Barclamp.find_key("network").id
+
+    # Find the barclamp config
+    deployment = Deployment.find_key(deployment_id)
+    return [404, "There is no Deployment with id #{deployment_id}"] if deployment.nil?
+
+    # If there is no proposed, then return the active one instead
+    snapshot_type = ACTIVE_SNAPSHOT if snapshot_type == PROPOSED_SNAPSHOT && deployment.proposed_snapshot.nil?
+    snapshot = (snapshot_type == ACTIVE_SNAPSHOT ? deployment.active :  deployment.proposed)
+
+    return [404, "There is no active snapshot"] if snapshot.nil?
+
+    begin
+      networks = BarclampNetwork::Network.find_all_by_snapshot_id(snapshot.id)
+    rescue ActiveRecord::RecordNotFound => ex
+      return [404, ex.message]
+    end
+
+    if networks.nil? || networks.size == 0
+      return [400, "Deployment/Instance #{log_name(deployment)} doesn't have any networks."]
+    end
+
+    [200, networks]
+  end
 
 
   def self.log_name(object)
