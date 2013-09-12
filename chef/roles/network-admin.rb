eff6c6e04cf41da6106914e058c82b9150f3c80e
Victor Lowther <victor.lowther@gmail.com>
2013-09-12 14:29:14 -0500

    Prune roles that are no longer relavent.

--- chef/roles/network-admin.rb
+++
@@ -1,6 +0,0 @@
-name "network-admin"
-description "Manages the admin network."
-run_list("recipe[network]")
-host_name = %x{hostname}
-default_attributes()
-override_attributes()
--- chef/roles/network-server.rb
+++
@@ -1,9 +0,0 @@
-name "network-server"
-description "Network role - Setups the network"
-run_list()
-# These attributes are for scaffolding purposes ONLY!
-# Once the crowbar framework side of things is functional, they
-# need to go away.
-host_name = %x{hostname}
-default_attributes()
-override_attributes()
