3d828e618b4dee1d6c242a72c968dd9c92f0875a
Rick Salevsky <rsalevsky@suse.de>
2015-05-11 16:30:37 +0200

    Set MTU on device

--- chef/cookbooks/network/recipes/default.rb
+++ chef/cookbooks/network/recipes/default.rb
@@ -249,6 +249,10 @@ node["crowbar"]["network"].keys.sort{|a,b|
     our_iface = br
     net_ifs << our_iface.name
   end
+  if network["mtu"]
+    Chef::Log.info("setting mtu #{network['mtu']} on #{our_iface.name}")
+    ifs[our_iface.name]["mtu"] = network["mtu"]
+  end
   # Make sure our addresses are correct
   if_mapping[name] = net_ifs
   ifs[our_iface.name]["addresses"] ||= Array.new
